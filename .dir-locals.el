b669589b6b64e48027ea15c295d5192ccc174222
somuthink <qould12@gmail.com>
2025-10-12 00:58:48 +0500

    datomic development helper and forgetten gitignore part

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,11 @@
-((nil . ((cider-clojure-cli-aliases . ":dev:portal"))))
+((nil .
+      ((cider-clojure-cli-aliases . ":dev:portal")
+       (eval . (defun datomic.sqlite/conn ()
+  "Connect to the Datomic SQLite database in ./storage/sqlite.db"
+  (interactive)
+  (let ((ns (cider-current-ns)))
+    (cider-nrepl-sync-request:eval
+     (format "(in-ns '%s)
+              (def conn (d/connect \"datomic:sql://app?jdbc:sqlite:./storage/sqlite.db\"))"
+             ns))))))))
+
